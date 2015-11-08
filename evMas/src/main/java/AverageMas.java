68decb6859e31c28225c754e1ea0da257326be28
Ann <anna.chumak1409@gmail.com>
2015-11-08 10:25:04 +0200

    Task_2

--- evMas/src/main/java/AverageMas.java
+++ evMas/src/main/java/AverageMas.java
@@ -1,6 +1,22 @@
 /**
  * Created by Anna on 07.11.2015.
  */
+/*Найдите среднее арифметическое массива из 10 элементов типа double.*/
 public class AverageMas {
-    
+    public static void main(String [] args){
+        double[] mas = new double[10];
+        double mas_sum = 0;
+        double mas_aver = 0;
+
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = Math.round(Math.random() * 100);
+            System.out.println(mas[i]);
+
+            mas_sum += mas[i];
+        }
+        System.out.println (mas_sum);
+        mas_aver = mas_sum/mas.length;
+        System.out.println (mas_aver);
+    }
+
 }
